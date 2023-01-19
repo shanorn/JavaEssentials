a243045cd453580f22e9ac4298a64c4c0bad2e9d
shanorn <shanorn@gmail.com>
2023-01-19 23:54:40 +0100

    Ejercicios 01-02-03

---
+++ Aprende Java con Ejercicios/Tema 1. Salida por pantalla/Ejercicio 02/Ejercicio02/src/main/java/com/melonsoft/ejercicio02/Ejercicio02.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.melonsoft.ejercicio02;
+
+/**
+ *
+ * @author zerox
+ */
+public class Ejercicio02 {
+
+    public static void main(String[] args) {
+        System.out.println("Nombre: Cristian");
+        System.out.println("Dirección: Av. Los Toreros 2, 1ºE");
+        System.out.println("Teléfono: 633024689");
+        
+    }
+}
