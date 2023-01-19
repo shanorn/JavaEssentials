40937bcb9022858ba11c04f600ac61c4b0557240
shanorn <shanorn@gmail.com>
2023-01-19 23:54:31 +0100

    Ejercicios 01-02-03

---
+++ Aprende Java con Ejercicios/Tema 1. Salida por pantalla/Ejercicio 01/Ejercicio01/src/main/java/com/melonsoft/ejercicio01/Ejercicio01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.melonsoft.ejercicio01;
+
+/**
+ *
+ * @author zerox
+ */
+public class Ejercicio01 {
+
+    public static void main(String[] args) {
+        System.out.println("Cristian");
+    }
+}
