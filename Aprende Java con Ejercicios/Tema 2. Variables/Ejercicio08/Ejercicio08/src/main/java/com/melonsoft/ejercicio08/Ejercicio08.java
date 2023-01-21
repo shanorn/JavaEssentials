ce9cdeb966f428a585f334631ac31c76403b38d1
shanorn <shanorn@gmail.com>
2023-01-21 20:02:41 +0100

    IVA

---
+++ Aprende Java con Ejercicios/Tema 2. Variables/Ejercicio08/Ejercicio08/src/main/java/com/melonsoft/ejercicio08/Ejercicio08.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.melonsoft.ejercicio08;
+
+/**
+ *
+ * @author zerox
+ */
+public class Ejercicio08 {
+
+    public static void main(String[] args) {
+        char letra1 = 'C';
+        char letra2 = 'r';
+        char letra3 = 'i';
+        char letra4 = 's';
+        char letra5 = 't';
+        char letra6 = 'i';
+        char letra7 = 'a';
+        char letra8 = 'n';
+        String nombre = "" + letra1 + letra2 + letra3 + letra4 + letra5 + letra6 + letra7 + letra8;
+        System.out.println(nombre);
+    }
+}
