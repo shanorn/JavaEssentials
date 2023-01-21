e7b80b2a589d497c02a0610fec340625c5e716fb
shanorn <shanorn@gmail.com>
2023-01-21 19:20:20 +0100

    Conversor €/Pesetas

---
+++ Aprende Java con Ejercicios/Tema 2. Variables/Ejercicio04/Ejercicio04/src/main/java/com/melonsoft/ejercicio04/Ejercicio04.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.melonsoft.ejercicio04;
+
+/**
+ *
+ * @author zerox
+ */
+public class Ejercicio04 {
+
+    public static void main(String[] args) {
+        double eurosPorPeseta = 0.006010121043837823;
+        double pesetasPorEuro = 166.386;
+        
+        double euros = 6;
+        double pesetas = 100;
+        
+        System.out.println(euros + " son " + euros * pesetasPorEuro + " pesetas.");
+        System.out.println(pesetas + " son " + pesetas * eurosPorPeseta + " Euros.");
+    }
+}
