be5fe66ff89fe5f0d42b30de34faa52bb498d850
shanorn <shanorn@gmail.com>
2023-01-21 19:12:54 +0100

    Nombre

---
+++ Aprende Java con Ejercicios/Tema 2. Variables/Ejercicio02-03/Ejercicio0203/src/main/java/com/melonsoft/ejercicio0203/Ejercicio0203.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.melonsoft.ejercicio0203;
+
+/**
+ *
+ * @author zerox
+ */
+public class Ejercicio0203 {
+
+    public static void main(String[] args) {
+        String nombre = "Critian";
+        String direccion = "C/ La Pera 2, 5ºT";
+        String telefono = "+(34) 600 000 123";
+        
+        System.out.println("Me llamo " + nombre + " y vivo en " + direccion + ". Puedes llamarme al siguiente número: " + telefono);
+    }
+}
