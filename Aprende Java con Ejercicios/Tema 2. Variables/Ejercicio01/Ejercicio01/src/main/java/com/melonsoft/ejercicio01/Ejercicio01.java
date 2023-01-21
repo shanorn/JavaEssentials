128e54faa4f6c0f4a29fd49958dade6d16a755e4
shanorn <shanorn@gmail.com>
2023-01-21 19:12:38 +0100

    Horario

---
+++ Aprende Java con Ejercicios/Tema 2. Variables/Ejercicio01/Ejercicio01/src/main/java/com/melonsoft/ejercicio01/Ejercicio01.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.melonsoft.ejercicio01;
+
+/**
+ *
+ * @author zerox
+ */
+public class Ejercicio01 {
+
+    public static void main(String[] args) {
+        int x = 144;
+        int y = 999;
+        System.out.println("x: " + x + " y:" + y);
+        System.out.println("Suma: " + (x + y));
+        System.out.println("Resta: " + (x - y));
+        System.out.println("Multiplicación: " + (x * y));
+        System.out.println("División (Entera): " + (x / y));
+        System.out.println("División (Decimal): " + ((double) x / (double) y));
+        System.out.println("Resto: " + (x % y));
+    }
+}
