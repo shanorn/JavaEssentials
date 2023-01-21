7ce2a1e3662a9c532bcd60de3328145282e0f6ee
shanorn <shanorn@gmail.com>
2023-01-21 19:51:30 +0100

    IVA

---
+++ Aprende Java con Ejercicios/Tema 2. Variables/Ejercicio07/Ejercicio07/src/main/java/com/melonsoft/ejercicio07/Ejercicio07.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.melonsoft.ejercicio07;
+
+/**
+ *
+ * @author zerox
+ */
+public class Ejercicio07 {
+
+    public static void main(String[] args) {
+        String texto = "Hola me llamo ";
+        char letra1 = 'C';
+        char letra2 = 'r';
+        char letra3 = 'i';
+        char letra4 = 's';
+        char letra5 = 't';
+        char letra6 = 'i';
+        char letra7 = 'a';
+        char letra8 = 'n';
+        
+        System.out.println(texto + letra1 + letra2 + letra3 + letra4 + letra5 + letra6 + letra7 + letra8);
+    }
+}
