df903c97293dd0a8d663770831e69490d44d6dc6
shanorn <shanorn@gmail.com>
2023-01-21 18:47:05 +0100

    Horario

--- Aprende Java con Ejercicios/Tema 1. Salida por pantalla/Ejercicio 04/Ejercicio04/src/main/java/com/melonsoft/ejercicio04/Ejercicio04.java
+++ Aprende Java con Ejercicios/Tema 1. Salida por pantalla/Ejercicio 04/Ejercicio04/src/main/java/com/melonsoft/ejercicio04/Ejercicio04.java
@@ -4,6 +4,8 @@
 
 package com.melonsoft.ejercicio04;
 
+import java.nio.charset.Charset;
+
 /**
  *
  * @author zerox
@@ -11,6 +13,24 @@ package com.melonsoft.ejercicio04;
 public class Ejercicio04 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        System.out.println("Horario:"); 
+        System.out.println("+----------+----------+----------+----------+----------+----------+");
+        System.out.printf("|%10s|%-10s|%-10s|%-10s|%-10s|%-10s|\n", "", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes");
+        System.out.println("+----------+----------+----------+----------+----------+----------+");
+        System.out.printf("|%10s|%-10s|%-10s|%-10s|%-10s|%-10s|\n", "8:15", "BBDD", "P. ETC", "OGE", "MTP II", "MTP I");
+        System.out.println("+----------+----------+----------+----------+----------+----------+");
+        System.out.printf("|%10s|%-10s|%-10s|%-10s|%-10s|%-10s|\n", "9:15", "BBDD", "P. ETC", "OGE", "MTP II", "MTP I");
+        System.out.println("+----------+----------+----------+----------+----------+----------+");
+        System.out.printf("|%10s|%-54s|\n","10:15", "                      Recreo");
+        System.out.println("+----------+----------+----------+----------+----------+----------+");
+        System.out.printf("|%10s|%-10s|%-10s|%-10s|%-10s|%-10s|\n", "10:45", "MTP I", "P. MTP I", "BBDD", "P. MTP II", "SSOO");
+        System.out.println("+----------+----------+----------+----------+----------+----------+");
+        System.out.printf("|%10s|%-10s|%-10s|%-10s|%-10s|%-10s|\n", "11:45", "SSOO", "P. MTP I", "BBDD", "P. MTP II", "OGE");
+        System.out.println("+----------+----------+----------+----------+----------+----------+");
+        System.out.printf("|%10s|%-10s|%-10s|%-10s|%-10s|%-10s|\n", "12:45", "SSOO", "P. MTP I", "SSOO", "", "OGE");
+        System.out.println("+----------+----------+----------+----------+----------+----------+");
+        System.out.printf("|%10s|%-10s|%-10s|%-10s|%-10s|%-10s|\n", "13:45", "ETC", "", "SSOO", "", "BBDD");
+        System.out.println("+----------+----------+----------+----------+----------+----------+");
     }
 }
